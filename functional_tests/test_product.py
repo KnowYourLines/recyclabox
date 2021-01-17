92776dfbb680de38ddf41b4b427e3f1745bc66c6
KnowYourLines <knowyourlines@outlook.com>
2021-01-17 21:35:59 +0000

    lists all available products

--- functional_tests/test_product.py
+++ functional_tests/test_product.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 
 from rest_framework.test import APISimpleTestCase
 
+from inventory.models import Product
+
 
 class ProductIntegrationTest(APISimpleTestCase):
     databases = {"default"}
@@ -15,3 +17,35 @@ class ProductIntegrationTest(APISimpleTestCase):
         }
         response = self.client.post("/inventory/", new_product, format="json")
         assert response.status_code == HTTPStatus.CREATED
+        assert response.data == {
+            "sku": "2346",
+            "name": "Apple iPhone 5",
+            "quantity": 5,
+            "price": 59.99,
+        }
+
+    def test_retrieve_product_by_sku(self):
+        created_product = Product(sku="hello", name="world", quantity=5, price=59.99)
+        created_product.save()
+        response = self.client.get("/inventory/hello/")
+        assert response.status_code == HTTPStatus.OK
+        assert response.data == {
+            "sku": "hello",
+            "name": "world",
+            "quantity": 5,
+            "price": 59.99,
+        }
+
+    def test_retrives_all_available_products(self):
+        created_product = Product(sku="2346", name="world", quantity=5, price=59.99)
+        created_product.save()
+        created_product = Product(sku="hello", name="world", quantity=5, price=59.99)
+        created_product.save()
+        created_product = Product(sku="goodbye", name="world", quantity=0, price=59.99)
+        created_product.save()
+        response = self.client.get("/inventory/available/")
+        assert response.status_code == HTTPStatus.OK
+        assert list(response.data) == [
+            {"name": "world", "quantity": 5, "price": 59.99, "sku": "2346"},
+            {"name": "world", "quantity": 5, "price": 59.99, "sku": "hello"},
+        ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,6 @@
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from inventory.models import Product
 from inventory.serializers import ProductSerializer
@@ -6,7 +8,12 @@ from inventory.serializers import ProductSerializer
 
 class InventoryViewSet(
     mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
     viewsets.GenericViewSet,
 ):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=False, methods=["get"])
+    def available(self, request):
+        return Response(self.get_queryset().filter(quantity__gt=0).values())
